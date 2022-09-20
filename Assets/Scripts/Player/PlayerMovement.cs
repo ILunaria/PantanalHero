ebdd7716885a12d3a3e057cb24f938996b8c1db3
Anderson Luiz <andersonluizbazzotti@gmail.com>
2022-09-20 17:31:13 -0400

    Criação de Cenas / Script Jump em Construção
    1. Criei uma cena para cada participante do projeto.
    
    2. Comecei a trabalhar no Jump Script utilizando o Input System da Unity.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float jumpForce;
+    public bool isGrounded = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        // DO COYOTE TIME AND JUMP BUFFERING
+        if(context.performed && isGrounded)
+        {
+            Debug.Log("Jump! " + context.phase);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+        rb.gravityScale = 3f; // TO DO BETTER
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+        rb.gravityScale = 1f; // TO DO BETTER
+    }
+}
