a440bc3328fa039c32582b917934f95c6ac6b6e7
Anderson Luiz <andersonluizbazzotti@gmail.com>
2022-09-22 15:51:30 -0400

    Dash + Better Jump
    1. Dash aplicado
    2. Jump melhorado
    3. Movimentação melhorada

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -1,35 +1,105 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData")]
 public class PlayerData : ScriptableObject
 {
-    [Header("Movement Presets")]
-    public float moveSpeed;
-    public float acceleration;
-    public float desacceleration;
+    #region GRAVITY
+    [Header("Gravity")]
+	[HideInInspector] public float gravityStrength; //Downwards force (gravity) needed for the desired jumpHeight and jumpTimeToApex.
+	[HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
+												 //Also the value the player's rigidbody2D.gravityScale is set to.
+	[Space(5)]
+	public float fallGravityMult; //Multiplier to the player's gravityScale when falling.
+	public float maxFallSpeed; //Maximum fall speed (terminal velocity) of the player when falling.
+	[Space(5)]
+	public float fastFallGravityMult; //Larger multiplier to the player's gravityScale when they are falling and a downwards input is pressed.
+									  //Seen in games such as Celeste, lets the player fall extra fast if they wish.
+	public float maxFastFallSpeed; //Maximum fall speed(terminal velocity) of the player when performing a faster fall.
 
-    [Space(5)]
+	[Space(20)]
+    #endregion
 
-    [Header("Jump Presets")]
-    public float jumpForce;
-    public float onGroundGravity;
-    public float fallGravityMultiplier;
+    #region RUN
+    [Header("Run")]
+	public float runMaxSpeed; //Target speed we want the player to reach.
+	public float runAcceleration; //The speed at which our player accelerates to max speed, can be set to runMaxSpeed for instant acceleration down to 0 for none at all
+	[HideInInspector] public float runAccelAmount; //The actual force (multiplied with speedDiff) applied to the player.
+	public float runDecceleration; //The speed at which our player decelerates from their current speed, can be set to runMaxSpeed for instant deceleration down to 0 for none at all
+	[HideInInspector] public float runDeccelAmount; //Actual force (multiplied with speedDiff) applied to the player .
+	[Space(5)]
+	[Range(0f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
+	[Range(0f, 1)] public float deccelInAir;
+	[Space(5)]
+	public bool doConserveMomentum = true;
 
-    [Space(5)]
+	[Space(20)]
+    #endregion
 
-    [Header("Ground Check")]
-    public LayerMask groundLayer;
-    public Vector2 groundCheckSize;
+    #region JUMP
+    [Header("Jump")]
+	public float jumpHeight; //Height of the player's jump
+	public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
+	[HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+    #endregion
 
-    [Space(5)]
+    #region JUMP EFFECTS
+    [Header("Jump Effects")]
+	public float jumpCutGravityMult; //Multiplier to increase gravity if the player releases thje jump button while still jumping
+	[Range(0f, 1)] public float jumpHangGravityMult; //Reduces gravity while close to the apex (desired max height) of the jump
+	public float jumpHangTimeThreshold; //Speeds (close to 0) where the player will experience extra "jump hang". The player's velocity.y is closest to 0 at the jump's apex (think of the gradient of a parabola or quadratic function)
+	[Space(0.5f)]
+	public float jumpHangAccelerationMult;
+	public float jumpHangMaxSpeedMult;
 
-    [Header("Coyote Time")]
-    public float coyoteTime;
+	[Space(20)]
+    #endregion
 
-    [Space(5)]
+    #region JUMP ASSISTS
+    [Header("Jump Assists")]
+	[Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
+	[Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
 
-    [Header("Jump Buffering")]
-    public float jumpBufferTime;
+	[Space(20)]
+    #endregion
+
+    #region DASH
+    [Header("Dash")]
+	public int dashAmount;
+	public float dashSpeed;
+	public float dashSleepTime; //Duration for which the game freezes when we press dash but before we read directional input and apply a force
+	[Space(5)]
+	public float dashAttackTime;
+	[Space(5)]
+	public float dashEndTime; //Time after you finish the inital drag phase, smoothing the transition back to idle (or any standard state)
+	public Vector2 dashEndSpeed; //Slows down player, makes dash feel more responsive (used in Celeste)
+	[Range(0f, 1f)] public float dashEndRunLerp; //Slows the affect of player movement while dashing
+	[Space(5)]
+	public float dashRefillTime;
+	[Space(5)]
+	[Range(0.01f, 0.5f)] public float dashInputBufferTime;
+    #endregion
+
+
+    //Unity Callback, called when the inspector updates
+    private void OnValidate()
+	{
+		//Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2) 
+		gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+
+		//Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
+		gravityScale = gravityStrength / Physics2D.gravity.y;
+
+		//Calculate are run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
+		runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
+		runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+		//Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
+		jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+
+		#region Variable Ranges
+		runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+		runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+		#endregion
+	}
+
 }
