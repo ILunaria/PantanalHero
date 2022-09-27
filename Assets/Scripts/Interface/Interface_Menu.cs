e6bddf498e2b51d8f2ba77938f661154710d27be
PedrinDev <pedro.silva25@estudante.ifms.edu.br>
2022-09-26 22:49:32 -0400

    Programa_Interface
    começo da codificação da interface

---
+++ Assets/Scripts/Interface/Interface_Menu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class Interface_Menu : MonoBehaviour
+{
+    public Button NovoJogo;
+    public Button Continuar;
+    public Button Sair;
+    public Button Config;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        NovoJogo = root.Q<Button>("Novojogo");
+        Continuar = root.Q<Button>("Continuar");
+        Config = root.Q<Button>("Configuracao");
+        Sair = root.Q<Button>("Sair");
+
+        NovoJogo.clicked += NovoJogoPressed;
+        Continuar.clicked += ContinuarPressed;
+        Sair.clicked += SairPressed;
+        
+    }
+
+   void NovoJogoPressed() 
+   {
+       SceneManager.LoadScene("Anderson_Scene");
+   }
+
+   void ContinuarPressed() 
+   {
+       Debug.Log("continua");
+   }
+
+   void SairPressed()
+   {
+       Application.Quit();
+       Debug.Log("Saiu");
+   }
+
+
+}
